337477a7a85a91a391b4521dd55f51d37cdd79f2
Mishell C. <jose.catota@snappcorp.com>
2026-01-18 19:19:37 -0500

    first commit

--- src/main/java/org/sofkajavajunior/service/ReporteService.java
+++
@@ -1,9 +0,0 @@
-package org.sofkajavajunior.service;
-
-import org.sofkajavajunior.dto.respuestaBase.BaseResponseDTO;
-
-import java.util.Date;
-
-public interface ReporteService {
-    BaseResponseDTO obtenerTodasLasCuentasSegunClienteYFecha(Date fechaInicio, Date fechaFin, Long clienteId);
-}
